2f5374254ff39c082bba3aaadff2c58cfda3acc9
CauaSDO <cauasilva@icloud.com>
2025-04-10 23:29:50 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// aula 1 curso js
+        var hora = new Date().getHours();
+        var saudação;
+        
+        
+        if(hora < 12){
+            saudação = "Bom dia!";
+        }
+        else if (hora < 18){
+            saudação = "Boa tarde!";
+        }
+        else {
+            saudação = "Boa Noite!";
+        }
+
+        document.getElementById("mensagem").innerHTML = 
+            saudação;
+
+
+// aula 2 curso js
+
+    //Print
+
+            console.log("quebre 3 ovos");
+            console.log("misture com a farinha");
+            console.log("leve ao forno");
+        
+            {
+                console.log("coma 1 pedaço de pizza");
+            }
+        
+
+// aula 3 curso js
+
+/*
+Regras declaração das váriaveis
+
+1) não inicializadas com números, apenas letras ou underlines
+2) não utilizar caractéres especiais
+3) não pode utilizar palavras reservadas da linguagem
+*/
+
+//variaveis  
+    //let 
+    //var 
+//constante
+    //const 
+
+            let altura = 1.90;
+            let peso = 70;
+            const pi = 3.14;
+
+            let resultado = peso / (altura * altura);
+            let resultado2 = pi * 2
+
+
+//aula 4 curso js
+
+    //tipagem dinâmica
+
+            let nome = "Cauã" //String
+            let numero = 1200 //Inteiros 
+            let decimal = 1.25 //Decimais
+            let negativo = -1200 //Negativos
+            let avançarProximoEpisodio = true //Booleanos
+
+
