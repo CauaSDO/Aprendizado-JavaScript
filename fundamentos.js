bfda06e0505dd2060d152054d02783bf328b4b38
CauaSDO <cauasilva@icloud.com>
2025-04-11 01:23:48 -0300

    Operadores e Funções JavaScript

---
+++ fundamentos.js
@@ -0,0 +1,54 @@
+
+// aula 1 curso js
+
+    //Print
+
+            //console.log("quebre 3 ovos");
+            //console.log("misture com a farinha");
+            //console.log("leve ao forno");
+        
+            //{
+                //console.log("coma 1 pedaço de pizza");
+            //}
+        
+
+
+// aula 2 curso js
+
+/* Regras declaração das váriaveis
+
+1) não inicializadas com números, apenas letras ou underlines
+2) /não utilizar caractéres especiais
+3) não pode utilizar palavras reservadas da linguagem
+
+*/
+
+//variaveis  
+    //let 
+    //var 
+
+//constante
+    //const 
+
+            //let altura = 1.90;
+            //let peso = 70;
+            //const pi = 3.14;
+
+            //let resultado = peso / (altura * altura);
+            //let resultado2 = pi * 2
+
+
+
+//aula 3 curso js
+
+    //tipagem dinâmica
+
+            //let nome = "Cauã" //String
+            //let numero = 1200 //Inteiros 
+            //let decimal = 1.25 //Decimais
+            //let negativo = -1200 //Negativos
+            //let avançarProximoEpisodio = true //Booleanos
+
+
+
+
